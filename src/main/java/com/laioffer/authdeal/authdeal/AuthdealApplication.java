c574b4b83b34802a6571762ca2a5972bbfce881c
anson <zhang.yang6@husky.neu.edu>
2021-02-07 09:40:52 -0800

    First Commit.

---
+++ src/main/java/com/laioffer/authdeal/authdeal/AuthdealApplication.java
@@ -0,0 +1,13 @@
+package com.laioffer.authdeal.authdeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthdealApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AuthdealApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/laioffer/authdeal/authdeal/AuthdealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laioffer.authdeal.authdeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthdealApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
