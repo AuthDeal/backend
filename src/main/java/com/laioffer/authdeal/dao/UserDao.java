5006fbf98420ef7dc692c7af8910524a0fcab54d
anson <zhang.yang6@husky.neu.edu>
2021-02-14 20:09:38 -0800

    Adds User repo, dao, and controller.

---
+++ src/main/java/com/laioffer/authdeal/dao/UserDao.java
@@ -0,0 +1,30 @@
+package com.laioffer.authdeal.dao;
+
+import com.laioffer.authdeal.entity.Users;
+import com.laioffer.authdeal.repository.UsersRepository;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserDao {
+  @Autowired
+  UsersRepository usersRepository;
+
+  public void createUsers(Users user) {
+    usersRepository.save(user);
+  }
+
+  public Users findUserById(String userName) {
+    return usersRepository.findById(userName).get();
+  }
+
+  public List<Users> findAllUsers() {
+    List<Users> result = new ArrayList<>();
+    for (Users user : usersRepository.findAll()) {
+      result.add(user);
+    }
+    return result;
+  }
+}
--- src/main/java/com/laioffer/authdeal/repository/UsersRepository.java
+++ src/main/java/com/laioffer/authdeal/repository/UsersRepository.java
@@ -3,6 +3,6 @@ package com.laioffer.authdeal.repository;
 import com.laioffer.authdeal.entity.Users;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UsersRepository extends CrudRepository<Users, Integer> {
+public interface UsersRepository extends CrudRepository<Users, String> {
 
 }
