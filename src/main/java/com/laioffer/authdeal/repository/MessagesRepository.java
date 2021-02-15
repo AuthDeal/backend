2b2450ec62de450b050451600225d9108047d1b8
anson <zhang.yang6@husky.neu.edu>
2021-02-14 19:16:42 -0800

    Adds repository.

---
+++ src/main/java/com/laioffer/authdeal/repository/MessagesRepository.java
@@ -0,0 +1,8 @@
+package com.laioffer.authdeal.repository;
+
+import com.laioffer.authdeal.entity.Messages;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MessagesRepository extends CrudRepository<Messages, Integer> {
+
+}
---
+++ src/main/java/com/laioffer/authdeal/repository/OrdersRepository.java
@@ -0,0 +1,8 @@
+package com.laioffer.authdeal.repository;
+
+import com.laioffer.authdeal.entity.Orders;
+import org.springframework.data.repository.CrudRepository;
+
+public interface OrdersRepository extends CrudRepository<Orders, Integer> {
+
+}
