e28530376a41a7d0bc4669dbb99aa5e950b49703
anson <zhang.yang6@husky.neu.edu>
2021-02-14 20:16:46 -0800

    Adds User repo, dao, and controller.

--- src/main/java/com/laioffer/authdeal/contoller/UserController.java
+++ src/main/java/com/laioffer/authdeal/contoller/UserController.java
@@ -5,6 +5,7 @@ import com.laioffer.authdeal.entity.Users;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,7 +14,7 @@ public class UserController {
   @Autowired
   private UserDao userDao;
 
-  @RequestMapping("/users/all")
+  @RequestMapping(value = "/users/all", method = RequestMethod.GET)
   public List<Users> fetchAllUsers() {
     return userDao.findAllUsers();
   }
