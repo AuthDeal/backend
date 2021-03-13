cdec556534d1815b505523452367cb3dfeef4883
anson <zhang.yang6@husky.neu.edu>
2021-03-13 11:16:28 -0800

    Re-initial anson branch

--- src/main/java/com/laioffer/authdeal/controller/HelloController.java
+++ src/main/java/com/laioffer/authdeal/controller/HelloController.java
@@ -1,12 +1,16 @@
 package com.laioffer.authdeal.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
+@CrossOrigin
 public class HelloController {
-  @RequestMapping("/")
+  @RequestMapping(value = "/", method = RequestMethod.GET)
   public String index() {
-    return "Greetings from Spring Boot!";
+    return "index.html?page='a'";
   }
 }
--- src/main/java/com/laioffer/authdeal/controller/UserController.java
+++ src/main/java/com/laioffer/authdeal/controller/UserController.java
@@ -1,21 +1,55 @@
 package com.laioffer.authdeal.controller;
 
 import com.laioffer.authdeal.dao.UserDao;
+import com.laioffer.authdeal.entity.Result;
 import com.laioffer.authdeal.entity.Users;
 import java.util.List;
+import java.util.UUID;
+import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class UserController {
 
   @Autowired
   private UserDao userDao;
 
+  @RequestMapping(value = "/users/register", method={RequestMethod.POST, RequestMethod.GET})
+  public Result register(Users user) {
+    return userDao.register(user);
+  }
+
+  @RequestMapping(value = "/users/login", method={RequestMethod.POST, RequestMethod.GET})
+  public Result login(Users user) {
+    return userDao.login(user);
+  }
+
   @RequestMapping(value = "/users/all", method = RequestMethod.GET)
   public List<Users> fetchAllUsers() {
     return userDao.findAllUsers();
   }
+
+  @GetMapping("/userInfo")
+  public String userInfo(HttpSession session) {
+    //从session获取User对象
+    Users user = (Users) session.getAttribute("user");
+    //如果用户为空，则创建新的对象
+    if (user == null) {
+      user = new Users();
+      user.setUserName(UUID.randomUUID().toString());
+      //设置默认头像
+      String picture = "/0/D/359EC8DE4BE24833A4BAFA98136E0A67.jpeg";
+      user.setPicture(picture);
+      session.setAttribute("user",user);
+    }
+    //否则直接进入用户信息页面
+    return "userInfo";
+  }
+
  }
